8e2d43c5cd98ee3139ec17fd2f8e2a9dbe859692
Fintan Kelly <B00099936@mytudublin.ie>
2026-03-20 16:22:25 +0000

    Fix again

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_labs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
 app = application
